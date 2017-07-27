3e52697a5b0561cf644a207ccd9b7292f20c3116
YG <1610098853@qq.com>
2017-07-27 10:38:50 +0800

    updata

--- app/src/main/java/com/yg/mycustomcontrols/MainActivity.java
+++ app/src/main/java/com/yg/mycustomcontrols/MainActivity.java
@@ -12,11 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LinearLayout linearLayout=(LinearLayout)findViewById(R.id.content);
-        MyVideoView mMyVideoView=new MyVideoView(this)
-                .inintVideoData()
-                .setVideoPath("https://img2.ch999img.com//pic/product/opic/201706162136041.mp4")
-                .loadeVideo();
-        linearLayout.addView(mMyVideoView.getVideoViewLayout());
     }
 }
